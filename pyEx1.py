770d00b242eb4ffc8cd6686028e1a996b7e998f4
Yehuda Dayan <dyehuda10@gmail.com>
2025-04-22 11:54:45 +0300

    dai

---
+++ pyEx1.py
@@ -0,0 +1,13 @@
+sentence = input("enter a sentence: ")
+length = len(sentence)
+print("The length of the sentence is", length)
+favorite_color = input("What is your favorite color? ")
+#favorite_color = it is a very siple name
+print("Your favorite color is", favorite_color)
+my_age = input("What is your age? ")
+#my_age = it is a very siple name
+print("Your age is", my_age)
+#my_age = it is a very siple name
+favorite_animal = input("What is your favorite animal? ")
+#favorite_animal = it is a very siple name
+print("Your favorite animal is", favorite_animal)
